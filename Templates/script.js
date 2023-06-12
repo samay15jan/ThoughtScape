f50310abf226b1e442622d0df525850018824d0e
samay15jan <samay15jan@gmail.com>
2023-06-13 00:32:15 +0530

    [commit]

--- Templates/script.js
+++ Templates/script.js
@@ -1,9 +1,16 @@
 function toggletheme(){
   var x = document.getElementById("theme");
+  var y = document.getElementById("toggle1")
+  var z = document.getElementById("toggle2")
   if (x.className === 'light-theme'){
     x.className = 'dark-theme';
+    y.style = ' width: 4%; color:white; background-color: transparent; border: none;';
+    z.src = '/sources/toggle_sun.svg'
   }
   else {
     x.className = 'light-theme';
+    y.style = 'width: 4%; color:black; background-color: transparent; border: none;';
+    z.src = '/sources/toggle_moon.svg'
+
   }
 }
