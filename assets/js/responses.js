db086a22e1c8d75598fd0fb10037d9763313b67a
samay15jan <samay15jan@gmail.com>
2023-07-23 23:21:58 +0530

    matterport feature

--- assets/js/responses.js
+++ assets/js/responses.js
@@ -8,7 +8,7 @@ async function response() {
       model: 'gpt-3.5-turbo',
       messages: [{ role: 'system', content: 'You Elena, an ai assistant who helps users by providing solutions for everyday problems.' }, { role: 'user', content: input }],
       temperature: 0.5,
-      max_tokens: 500,
+      max_tokens: 300,
       top_p: 1,
       frequency_penalty: 0.0,
       presence_penalty: 0.6,
@@ -25,7 +25,49 @@ async function response() {
   
     const data = await response.json();
     const message = data.choices[0].message.content;
-    const output = document.getElementById("center");
-    output.innerHTML = "AI:  " +  message;
+    var partition = document.getElementById("area");
+    let x = document.getElementById("theme");
+    if (x.className === 'dark-theme'){
+      partition.className = "bg-slate-800 text-white rounded-xl p-2 lg:ml-20 lg:ml-64 mx-10 w-4/5 lg:w-2/3 textarea drop-shadow-xl h-1/6";
+    }
+    else {
+      partition.className = "bg-white text-black rounded-xl p-2 lg:ml-20 lg:ml-64 mx-10 w-4/5 lg:w-2/3 textarea drop-shadow-xl h-1/6";
+    }
+    var i = 0;
+    var txt = "Elena: " + message;
+    var speed = 25;
+    function typeWriter() {
+      if (i < txt.length) {
+        const output = document.getElementById("AI-response");
+        let x = document.getElementById("theme");
+        if (x.className === 'light-theme'){
+          output.className = "animate-3 bg-white text-black rounded-xl p-2 mt-4 lg:ml-20 lg:ml-64 mx-10 w-4/5 lg:w-2/3 textarea drop-shadow-xl h-2/5";    
+          output.innerHTML += txt.charAt(i);
+          i++;
+          setTimeout(typeWriter, speed);
+        }
+        else {
+          output.className = "animate-3 bg-slate-800 text-white rounded-xl p-2 mt-4 lg:ml-20 lg:ml-64 mx-10 w-4/5 lg:w-2/3 textarea drop-shadow-xl h-2/5";    
+          output.innerHTML += txt.charAt(i);
+          i++;
+          setTimeout(typeWriter, speed);
+        }}
+      } 
+    typeWriter()
+    
+   
+    const sr = ScrollReveal({
+      distance: '100px', 
+      duration: 1000,   
+      easing: 'ease',   
+      mobile: true      
+    });
+    sr.reveal('.animate-3', {
+      origin: 'bottom',  
+      interval: 500    
+    })
 }
-  
+
+
+
+// typing effect
--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -8,17 +8,17 @@ function toggletheme(){
   let y = document.getElementById("toggle1")
   let z = document.getElementById("toggle2")
   if (x.className === 'light-theme'){
-    t.className = 'bottom-items css-button-sliding-to-top--black2'
-    u.className = 'bottom-items css-button-sliding-to-top--black2'
-    v.className = 'bg-gray-800 text-white rounded-xl p-2 lg:ml-20 lg:ml-64 mx-10 w-4/5 lg:w-2/3 textarea drop-shadow-xl h-3/5';
-    w.className = 'bg-gray-800 text-white rounded-xl p-2 lg:ml-20 lg:ml-64 lg:w-2/3 mx-10 w-4/5 textarea mb-4 drop-shadow-xl;';
+    t.className = 'lg:ml-96 css-button-sliding-to-top--black2'
+    u.className = 'ml-20 lg:ml-80 css-button-sliding-to-top--black2'
+    v.className = 'bg-slate-800 text-white rounded-xl p-2 lg:ml-20 lg:ml-64 mx-10 w-4/5 lg:w-2/3 textarea drop-shadow-xl h-3/5';
+    w.className = 'bg-slate-800 text-white rounded-xl p-2 lg:ml-20 lg:ml-64 lg:w-2/3 mx-10 w-4/5 textarea mb-4 drop-shadow-xl;';
     x.className = 'dark-theme';
     y.style = 'color:white; background-color: transparent; border: none;';
-    z.src = "/assets/images/toggle-sun.svg";
+    z.src = '/assets/images/toggle-sun.svg';
   }
   else {
-    t.className = 'bottom-items css-button-sliding-to-top--black1'
-    u.className = 'bottom-items css-button-sliding-to-top--black1'
+    t.className = 'lg:ml-96 css-button-sliding-to-top--black1'
+    u.className = 'ml-20 lg:ml-80 css-button-sliding-to-top--black1'
     v.className = 'bg-white rounded-xl p-2 lg:ml-20 lg:ml-64 mx-10 w-4/5 lg:w-2/3 textarea drop-shadow-xl h-3/5';
     w.className = 'bg-white rounded-xl p-2 lg:ml-20 lg:ml-64 lg:w-2/3 mx-10 w-4/5 textarea mb-4 drop-shadow-xl';
     x.className = 'light-theme';
@@ -26,3 +26,23 @@ function toggletheme(){
     z.src = '/assets/images/toggle-moon.svg';
   }
 }
+
+
+// Initialize ScrollReveal
+   const sr = ScrollReveal({
+    distance: '100px', 
+    duration: 1000,   
+    easing: 'ease',   
+    mobile: true      
+  });
+  
+  sr.reveal('.animate-1', {
+    origin: 'top',  
+    interval: 10     
+  })
+
+  sr.reveal('.animate-4', {
+    origin: 'bottom',  
+    interval: 10     
+  })
+
